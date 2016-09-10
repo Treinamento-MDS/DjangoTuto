836deebd9b546cea3b4301c73e8e554164124e0e
Renatafsouza <renata.rfsouza@gmail.com>
2016-09-10 09:44:19 -0300

    add imagem de fundo

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,11 +1,10 @@
-import datetime
+#from .models import datetime
 from django.test.utils import setup_test_environment
 from django.utils import timezone
 from django.test import TestCase
 from .models import Question
 
 setup_test_environment()
-
 class QuestionMethodTests(TestCase):
 
     def test_was_published_recently_with_future_question(self):
@@ -17,20 +16,25 @@ class QuestionMethodTests(TestCase):
         future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
 
-        def test_was_published_recently_with_old_question(self):
-    """
-    was_published_recently() should return False for questions whose
-    pub_date is older than 1 day.
-    """
-    time = timezone.now() - datetime.timedelta(days=30)
-    old_question = Question(pub_date=time)
-    self.assertIs(old_question.was_published_recently(), False)
+    def test_was_published_recently_with_old_question(self):
+        """
+        was_published_recently() should return False for questions whose
+        pub_date is older than 1 day.
+        """
+        time = timezone.now() - datetime.timedelta(days=30)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
 
-def test_was_published_recently_with_recent_question(self):
-    """
-    was_published_recently() should return True for questions whose
-    pub_date is within the last day.
-    """
-    time = timezone.now() - datetime.timedelta(hours=1)
-    recent_question = Question(pub_date=time)
-    self.assertIs(recent_question.was_published_recently(), True)
+    def test_was_published_recently_with_recent_question(self):
+        """
+        was_published_recently() should return True for questions whose
+        pub_date is within the last day.
+        """
+        time = timezone.now() - datetime.timedelta(hours=1)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
+
+client=Client()
+
+response = client.get('/')
+response.status_code
