b6ba36d91f14ec45b50b7e8ee9ea19fb36a9aa01
Renatafsouza <renata.rfsouza@gmail.com>
2016-09-10 10:05:13 -0300

    melhorando  a interface

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
-from django.utils import timezone
-from polls.models import Question
-# create a Question instance with pub_date 30 days in the future
-# Register your models here.
-future_question = Question(pub_date=timezone.now() + datetime.timedelta(days=30))
-# was it published recently?
-future_question.was_published_recently()
+
+from .models import Choice, Question
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+admin.site.register(Question, QuestionAdmin)
