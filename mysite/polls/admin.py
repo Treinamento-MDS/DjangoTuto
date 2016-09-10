012231b1b912bb80c769b3450af5c03db400f9ba
Renatafsouza <renata.rfsouza@gmail.com>
2016-09-09 22:33:26 -0300

    verifica existencia de bug

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from django.utils import timezone
+from polls.models import Question
+# create a Question instance with pub_date 30 days in the future
 # Register your models here.
+future_question = Question(pub_date=timezone.now() + datetime.timedelta(days=30))
+# was it published recently?
+future_question.was_published_recently()
